babcaae627c9efaaec8b089f61ea8589e43d504f
Nils-Kammertoens <nils.kammertoens@gmail.com>
2026-05-30 02:37:07 +0200

    Update src/main/java/highlighting/presets/MiniJavaTokens.java
    Ergänze Keywords Pattern
    
    Co-authored-by: xSTYX19x <239771716+xSTYX19x@users.noreply.github.com>

--- src/main/java/highlighting/presets/MiniJavaTokens.java
+++ src/main/java/highlighting/presets/MiniJavaTokens.java
@@ -20,7 +20,14 @@ public final class MiniJavaTokens {
         Token.of(Pattern.compile("'([^'\\\\]|\\\\.)'"), MiniJavaColours.CHAR_LITERAL_COLOUR),
         // Keywords
         Token.of(
-            Pattern.compile("\\b(package|import|class|public|private|final|return|null|new)\\b"),
+           Token.of(
+            Pattern.compile(
+                "\\b(abstract|assert|boolean|break|byte|case|catch|char|class|const|continue"
+                    + "|default|do|double|else|enum|extends|final|finally|float|for|goto|if"
+                    + "|implements|import|instanceof|int|interface|long|native|new|package"
+                    + "|private|protected|public|return|short|static|strictfp|super|switch"
+                    + "|synchronized|this|throw|throws|transient|try|void|volatile|while"
+                    + "|true|false|null|var|record|sealed|yield)\\b"),
             MiniJavaColours.KEYWORD_COLOUR),
         // Annotations
         Token.of(Pattern.compile("@[A-Za-z-]+"), MiniJavaColours.ANNOTATION_COLOUR));
